b7c3c04d7fe562a8478184dbe9951523e891d048
BernardoHer <bernaher19@gmail.com>
2022-11-08 16:53:00 -0500

    first commit

--- 02.js
+++ 02.js
@@ -5,15 +5,13 @@
   No comentar la funcion 
 */
 function stringMasLarga(strings) {
-  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
-  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
-  // Ej:
-  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
-  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
-
-  // Tu código aca
-}
-
+  let stringgrande = [];
+  for (let i = 0; i < strings.length; i++){
+  if (strings [i] > stringgrande)
+  stringgrande = strings[i];
+  }
+   return stringgrande;
+ }
 // No modifiques nada debajo de esta linea //
 
 module.exports = stringMasLarga
