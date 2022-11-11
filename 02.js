46c5148ab3e67663ae704faffc09a4706a6ec906
BernardoHer <bernaher19@gmail.com>
2022-11-11 09:28:18 -0500

    fix 2

--- 02.js
+++ 02.js
@@ -4,23 +4,22 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-
 function stringMasLarga(strings) {
-  let mayor = [0];
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // Ej:
+  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
+
+  // Tu código aca
+  let mayor = strings[0];
   for (i = 1; i < strings.length; i++) {
     if (strings[i].length > mayor.length) {
       mayor = strings[i];
     }
   }
-  return mayor;
+  return mayor
 }
-// La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
-// y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
-// Ej:
-// stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
-// stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
-
-// Tu código aca
 // No modifiques nada debajo de esta linea //
 
 module.exports = stringMasLarga
